8cfab4926ba8c303d08e6270bda8b9ecffdd4e6e
Тома Ухинова <tatulya2001@gmail.com>
2020-04-11 00:50:00 +0300

    Добавьте файлы проекта.

---
+++ Laboratornaya_10/Laba10.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Laboratornaya_10
+{
+    partial class Laba10
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SizeLineLable = new System.Windows.Forms.Label();
+            this.CircleRadioButton = new System.Windows.Forms.RadioButton();
+            this.SquareRadioButton = new System.Windows.Forms.RadioButton();
+            this.TriangleRadioButton = new System.Windows.Forms.RadioButton();
+            this.SnowmanRadioButton = new System.Windows.Forms.RadioButton();
+            this.HouseRadioButton = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SizeLineUpDown = new System.Windows.Forms.NumericUpDown();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.MyColorDialog = new System.Windows.Forms.ColorDialog();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SizeLineUpDown)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ГРАФИКА В WINDOWS FORMS";
+            // 
+            // SizeLineLable
+            // 
+            this.SizeLineLable.AutoSize = true;
+            this.SizeLineLable.Location = new System.Drawing.Point(7, 13);
+            this.SizeLineLable.Name = "SizeLineLable";
+            this.SizeLineLable.Size = new System.Drawing.Size(96, 19);
+            this.SizeLineLable.TabIndex = 2;
+            this.SizeLineLable.Text = "Толщина линии";
+            // 
+            // CircleRadioButton
+            // 
+            this.CircleRadioButton.AutoSize = true;
+            this.CircleRadioButton.Location = new System.Drawing.Point(16, 15);
+            this.CircleRadioButton.Name = "CircleRadioButton";
+            this.CircleRadioButton.Size = new System.Drawing.Size(57, 23);
+            this.CircleRadioButton.TabIndex = 3;
+            this.CircleRadioButton.TabStop = true;
+            this.CircleRadioButton.Text = "Круг";
+            this.CircleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SquareRadioButton
+            // 
+            this.SquareRadioButton.AutoSize = true;
+            this.SquareRadioButton.Location = new System.Drawing.Point(16, 38);
+            this.SquareRadioButton.Name = "SquareRadioButton";
+            this.SquareRadioButton.Size = new System.Drawing.Size(114, 23);
+            this.SquareRadioButton.TabIndex = 4;
+            this.SquareRadioButton.TabStop = true;
+            this.SquareRadioButton.Text = "Прямоугольник";
+            this.SquareRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // TriangleRadioButton
+            // 
+            this.TriangleRadioButton.AutoSize = true;
+            this.TriangleRadioButton.Location = new System.Drawing.Point(16, 63);
+            this.TriangleRadioButton.Name = "TriangleRadioButton";
+            this.TriangleRadioButton.Size = new System.Drawing.Size(97, 23);
+            this.TriangleRadioButton.TabIndex = 5;
+            this.TriangleRadioButton.TabStop = true;
+            this.TriangleRadioButton.Text = "Треугольник";
+            this.TriangleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SnowmanRadioButton
+            // 
+            this.SnowmanRadioButton.AutoSize = true;
+            this.SnowmanRadioButton.Location = new System.Drawing.Point(16, 87);
+            this.SnowmanRadioButton.Name = "SnowmanRadioButton";
+            this.SnowmanRadioButton.Size = new System.Drawing.Size(77, 23);
+            this.SnowmanRadioButton.TabIndex = 6;
+            this.SnowmanRadioButton.TabStop = true;
+            this.SnowmanRadioButton.Text = "Снеговик";
+            this.SnowmanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // HouseRadioButton
+            // 
+            this.HouseRadioButton.AutoSize = true;
+            this.HouseRadioButton.Location = new System.Drawing.Point(16, 111);
+            this.HouseRadioButton.Name = "HouseRadioButton";
+            this.HouseRadioButton.Size = new System.Drawing.Size(53, 23);
+            this.HouseRadioButton.TabIndex = 7;
+            this.HouseRadioButton.TabStop = true;
+            this.HouseRadioButton.Text = "Дом";
+            this.HouseRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SnowmanRadioButton);
+            this.panel1.Controls.Add(this.HouseRadioButton);
+            this.panel1.Controls.Add(this.CircleRadioButton);
+            this.panel1.Controls.Add(this.SquareRadioButton);
+            this.panel1.Controls.Add(this.TriangleRadioButton);
+            this.panel1.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(137, 144);
+            this.panel1.TabIndex = 8;
+            // 
+            // SizeLineUpDown
+            // 
+            this.SizeLineUpDown.Location = new System.Drawing.Point(118, 11);
+            this.SizeLineUpDown.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.SizeLineUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.SizeLineUpDown.Name = "SizeLineUpDown";
+            this.SizeLineUpDown.ReadOnly = true;
+            this.SizeLineUpDown.Size = new System.Drawing.Size(120, 27);
+            this.SizeLineUpDown.TabIndex = 9;
+            this.SizeLineUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ColorButton);
+            this.panel2.Controls.Add(this.SizeLineUpDown);
+            this.panel2.Controls.Add(this.DrawButton);
+            this.panel2.Controls.Add(this.SizeLineLable);
+            this.panel2.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(155, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(252, 105);
+            this.panel2.TabIndex = 11;
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ColorButton.Location = new System.Drawing.Point(163, 58);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(75, 33);
+            this.ColorButton.TabIndex = 13;
+            this.ColorButton.Text = "Цвет";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DrawButton.Location = new System.Drawing.Point(11, 58);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(88, 33);
+            this.DrawButton.TabIndex = 12;
+            this.DrawButton.Text = "Нарисовать";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(413, 39);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(559, 406);
+            this.pictureBox.TabIndex = 13;
+            this.pictureBox.TabStop = false;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(250, 160);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 14;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Laba10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 469);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Laba10";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SizeLineUpDown)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label SizeLineLable;
+        private System.Windows.Forms.RadioButton CircleRadioButton;
+        private System.Windows.Forms.RadioButton SquareRadioButton;
+        private System.Windows.Forms.RadioButton TriangleRadioButton;
+        private System.Windows.Forms.RadioButton SnowmanRadioButton;
+        private System.Windows.Forms.RadioButton HouseRadioButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown SizeLineUpDown;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ColorDialog MyColorDialog;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ Laboratornaya_10/Laba10.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratornaya_10
+{
+    public partial class Laba10 : Form
+    {
+        private Color color = Color.Red;
+        public Laba10()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawCircle()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(color, (int)SizeLineUpDown.Value);
+            Brush brush = new SolidBrush(color);
+            graphics.DrawEllipse(pen, 180, 30, 300, 300);
+            graphics.FillEllipse(brush, 180, 30, 300, 300);
+            pictureBox.Image = bmp;
+        }
+
+        private void DrawRectangle()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(color, (int)SizeLineUpDown.Value);
+            Brush brush = new SolidBrush(color);
+            graphics.DrawRectangle(pen, 30, 30, 500, 300);
+            graphics.FillRectangle(brush, 30, 30, 500, 300);
+            pictureBox.Image = bmp;
+        }
+
+        private void DrawTriangle()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(color, (int)SizeLineUpDown.Value);
+            graphics.DrawLine(pen, 30, 376, 280, 30);
+            graphics.DrawLine(pen, 280, 30, 530, 376);
+            graphics.DrawLine(pen, 30, 376, 530, 376);
+            Brush brush = new SolidBrush(color);
+            Point[] point = new Point[3];
+            point[0] = new Point(30, 376);
+            point[1] = new Point(280, 30);
+            point[2] = new Point(530, 376);
+            graphics.DrawPolygon(pen, point);
+            graphics.FillPolygon(brush, point);
+            pictureBox.Image = bmp;
+        }
+
+        private void DrawSnowman()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(color, (int)SizeLineUpDown.Value);
+            Brush brush = new SolidBrush(color);
+            graphics.DrawEllipse(pen, 560 / 3-5, 406 / 2 - 10, 200, 200);
+            graphics.FillEllipse(brush, 560 / 3-5, 406 / 2 - 10, 200, 200);
+            graphics.DrawEllipse(pen, 530 / 3 + 44, 406 / 4 + 10, 120, 120);
+            graphics.FillEllipse(brush, 530 / 3 + 44, 406 / 4 + 10, 120, 120);
+            graphics.DrawEllipse(pen, 530 / 2 - 25, 50, 80, 80);
+            graphics.FillEllipse(brush, 530 / 2 - 25, 50, 80, 80);
+            graphics.DrawRectangle(pen, 530 / 2 - 5, 10, 40, 45);
+            graphics.FillRectangle(brush, 530 / 2 - 5, 10, 40, 45);
+            pictureBox.Image = bmp;
+        }
+
+        private void DrawHouse()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(color, (int)SizeLineUpDown.Value);
+            Brush brush = new SolidBrush(color);
+            Point[] point = new Point[3];
+            point[0] = new Point(30, 100);
+            point[1] = new Point(280, 30);
+            point[2] = new Point(530, 100);
+            graphics.DrawPolygon(pen, point);
+            graphics.FillPolygon(brush, point);
+            graphics.DrawRectangle(pen, 30, 105, 500, 290);
+            pictureBox.Image = bmp;
+
+        }
+
+        private void CheckFunction()
+        {
+            if (CircleRadioButton.Checked)
+            {
+                DrawCircle();
+                return;
+            }
+            else if (SquareRadioButton.Checked)
+            {
+                DrawRectangle();
+                return;
+            }
+            else if (TriangleRadioButton.Checked)
+            {
+                DrawTriangle();
+                return;
+            }
+            else if (SnowmanRadioButton.Checked)
+            {
+                DrawSnowman();
+                return;
+            }
+            else
+            {
+                DrawHouse();
+                return;
+            }
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            CheckFunction();
+        }
+
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyColorDialog = new ColorDialog();
+            if (MyColorDialog.ShowDialog() == DialogResult.OK)
+            {
+                color = MyColorDialog.Color;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.Clear(Color.Transparent);
+            pictureBox.Image = bmp;
+        }
+    }
+}
